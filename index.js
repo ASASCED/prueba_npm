f766df39d7507da5aad0c429003806062461369f
ASASCED <jesus_heaven117@outlook.com>
2019-09-02 10:08:39 -0500

    Improvement of the main function

--- index.js
+++ index.js
@@ -13,15 +13,18 @@ let auto = 0
 
 function stTxt (color, backgroundColor) {
   document.onmouseup = function () {
-    const element = document.createElement('span')
-    element.setAttribute('id', `stElement${auto}`)
-    window
-      .getSelection()
-      .getRangeAt(0)
-      .surroundContents(element)
-    const selectedElement = document.getElementById(`stElement${auto}`)
-    selectedElement.style.color = color
-    selectedElement.style.backgroundColor = backgroundColor
-    auto++
+    if (window.getSelection().toString().length > 0) {
+      const element = document.createElement('span')
+      element.setAttribute('id', `stElement${auto}`)
+      window
+        .getSelection()
+        .getRangeAt(0)
+        .surroundContents(element)
+      const selectedElement = document.getElementById(`stElement${auto}`)
+      selectedElement.style.color = color
+      selectedElement.style.backgroundColor = backgroundColor
+      auto++
+      window.getSelection().removeAllRanges()
+    }
   }
 }
