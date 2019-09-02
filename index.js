1e93a12a0127c9cb7bd14315b75fdee7ed94b7df
ASASCED <jesus_heaven117@outlook.com>
2019-09-02 16:42:54 -0500

    Prueba de npm nueva

--- index.js
+++ index.js
@@ -6,27 +6,24 @@ module.exports = {
    * @example
    * stTxt("WHITE", "ORANGE");
    */
-  stTxt,
-  stSize
+  st
 }
 
-let auto = 0
-
-function stTxt (color, backgroundColor) {
-  document.onmouseup = function () {
-    if (window.getSelection().toString().length > 0) {
-      const element = document.createElement('span')
-      element.setAttribute('id', `stElement${auto}`)
-      element.setAttribute('class', 'stElement')
-      window
-        .getSelection()
-        .getRangeAt(0)
-        .surroundContents(element)
-      const selectedElement = document.getElementById(`stElement${auto}`)
-      selectedElement.style.color = color
-      selectedElement.style.backgroundColor = backgroundColor
-      window.getSelection().removeAllRanges()
-      auto++
-    }
+const st = (() => {
+  const Constructor = (color, bgColor) => {
+    this.color = color
+    this.bgColor = bgColor
   }
-}
+
+  const init = (color, bgColor) => {
+    return new Constructor(color, bgColor)
+  }
+
+  Constructor.prototype.stColor = () => {
+    // Es obligatorio poner algun elemento con el id element de momento en lo que se agregan mas funciones
+    const element = document.getElementById('element')
+    element.style.backgroundColor = this.bgColor
+  }
+})()
+
+st('WHITE', 'ORANGE').stColor()
